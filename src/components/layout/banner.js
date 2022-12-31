c2f805b4e06bf3a3913e1cb796a92f81eb81e9df
mctin <mctin@hotmail.com>
2022-12-31 21:17:44 +0100

    changes made to footer banner and header on main pages

--- src/components/layout/banner.js
+++ src/components/layout/banner.js
@@ -7,7 +7,6 @@ const Banner = (props) => {
     // <div className="backgroundbanner">
     <div className="banner">
       <img className="imgban" src={props.img} alt="" />
-      <div className="overlay"></div>
       <h1 className="text"> {props.text}</h1>
     </div>
     // </div>
